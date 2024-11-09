67b90a407c27b6de3319115430c839b9511b9c40
Jiancheng LI <jiancheng.li@thoughtworks.com>
2024-11-10 02:05:08 +0800

    [CuteNumber] feat, create findAllEvenNumbers function

--- src/main/java/com/tw/Main.java
+++ src/main/java/com/tw/Main.java
@@ -1,7 +1,17 @@
 package com.tw;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {1, 3, 5, 8, 10, 15};
+        int[] evenNumbers = findAllEvenNumbers(numbers);
+        System.out.println(Arrays.toString(evenNumbers));
+    }
+
+    public static int[] findAllEvenNumbers(int[] numbers){
+        return Arrays.stream(numbers)
+                .filter( number -> number % 2 == 0)
+                .toArray();
     }
 }
